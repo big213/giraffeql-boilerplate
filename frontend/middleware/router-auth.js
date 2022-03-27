f93cbcc46911fabcfbc423eef64620754d401717
James Chang <jc4095@nyu.edu>
2022-03-27 12:20:57 -0400

    Remove unused console log in router-auth.js middleware

--- frontend/middleware/router-auth.js
+++ frontend/middleware/router-auth.js
@@ -1,7 +1,6 @@
 export default function ({ store, redirect, route }) {
   if (route.name === 'login' || route.name === 'register') {
     if (store.getters['auth/firebaseUser']) {
-      console.log('1')
       redirect('/')
     }
   } else if (!store.getters['auth/firebaseUser']) {
